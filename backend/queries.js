d387b96dc9ca29d86ac7f094e031e64229aa513d
Gudipaty Aniket <annibhai000@gmail.com>
2022-04-26 22:34:09 +0530

    system related queries

--- backend/queries.js
+++ backend/queries.js
@@ -48,12 +48,39 @@ where username = %s and node_id in %L;
 const AddAppToNode = `
 insert into node_application (node_id, application_name)
 values %L;
-`
+`;
 
 const DelAppFromNode = `
 delete from node_application
 where node_id = %d and application_name in %L;
-`
+`;
+
+
+const CPU_Usage = `from(bucket:"bucket")
+|> range(start: -5m)
+|> filter(fn: (r) => r._measurement == "cpu" and r._field == $1 and r.cpu == $2 and r.host = $3)`;
+
+const Memory = `from(bucket:"bucket")
+|> range(start: -5m)
+|> filter(fn: (r) => r._measurement == "mem" and r._field == $1 and r.host = $2)`;
+
+
+const Disk_Usage = `from(bucket:"bucket")
+|> range(start: -5m)
+|> filter(fn: (r) => r._measurement == "disk" and r._field == "used_percentage" and r.cpu == $1 and r.host = $2 and r.device = $3)`;
+
+const SYS_Info = `from(bucket:"bucket")
+|> range(start: -5m)
+|> filter(fn: (r) => r._measurement == "system" and r._field == $1 and r.host = $2)`;
+
+const Network = `from(bucket:"bucket")
+|> range(start: -5m)
+|> filter(fn: (r) => r._measurement == "net" and r._field == $1 and r.host = $2 and r.interface = $3)`;
+
+const Processes = `from(bucket:"bucket")
+|> range(start: -5m)
+|> filter(fn: (r) => r._measurement == "processes" and r._field == $1 and r.host = $2)`;
+
 
 module.exports = {
     RegUser,
