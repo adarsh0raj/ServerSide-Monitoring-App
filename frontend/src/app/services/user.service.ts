4a9fbb373c03354705976450d5e8479acc898f68
Adarsh <rajadarsh2602@gmail.com>
2022-04-28 15:42:26 +0530

    minorrrrrrrrrrrr

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -7,14 +7,14 @@ import { User } from '../interfaces/user';
 export class UserService {
     constructor(private http: HttpClient) { }
 
-    public apiUrl = 'http://localhost:4000';
+    public apiUrl = 'http://localhost:3080';
 
     getAll() {
         return this.http.get<User[]>(`${this.apiUrl}/users`);
     }
 
     register(user: User) {
-        return this.http.post(`${this.apiUrl}/users/register`, user);
+        return this.http.post(`${this.apiUrl}/register`, (user.username, user.password));
     }
 
     delete(id: number) {
